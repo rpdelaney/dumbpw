7a6b04a213878386efb9bca6456172ca53253301
Ryan Delaney <ryan.patrick.delaney@protonmail.com>
2021-12-07 14:26:08 -0500

    Add contracted argument value checking

--- dumbpw/pwgen.py
+++ dumbpw/pwgen.py
@@ -6,7 +6,22 @@ import deal
 @deal.has("random")
 @deal.pure
 @deal.raises()
+@deal.pre(
+    validator=lambda keyspace, pass_length: pass_length > 0,
+    message="pass_length must be greater than zero.",
+    exception=ValueError,
+)
+@deal.pre(
+    validator=lambda keyspace, pass_length: pass_length <= 512,
+    message="pass_length cannot be greater than 512.",
+    exception=ValueError,
+)
+@deal.pre(
+    validator=lambda keyspace, pass_length: len(keyspace) > 0,
+    message="keyspace have positive len.",
+    exception=ValueError,
+)
 def _generate(keyspace: str, pass_length: int) -> str:
     """Return a cryptographically secure password of length pass_length using
-    characters only from the given keyspace."""
+    characters only from the given keyspace. Max pass_length is 512."""
     return "".join(secrets.choice(keyspace) for i in range(pass_length))
