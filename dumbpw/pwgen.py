258f1d322f932307453bdc3332e421bf0339d9bd
Ryan Delaney <ryan.patrick.delaney@protonmail.com>
2021-12-09 19:10:56 -0500

    Rename keyspace to charspace
    The keyspace would be all the possible keys, or secrets.

--- dumbpw/pwgen.py
+++ dumbpw/pwgen.py
@@ -8,34 +8,34 @@ import deal
 @deal.pure
 @deal.raises()
 @deal.pre(
-    validator=lambda keyspace, pass_length: pass_length > 0,
+    validator=lambda charspace, pass_length: pass_length > 0,
     message="pass_length must be greater than zero.",
     exception=ValueError,
 )
 @deal.pre(
-    validator=lambda keyspace, pass_length: pass_length <= 512,
+    validator=lambda charspace, pass_length: pass_length <= 512,
     message="pass_length cannot be greater than 512.",
     exception=ValueError,
 )
 @deal.pre(
-    validator=lambda keyspace, pass_length: len("".join(keyspace)) > 0,
-    message="keyspace must have positive len.",
+    validator=lambda charspace, pass_length: len("".join(charspace)) > 0,
+    message="charspace must have positive len.",
     exception=ValueError,
 )
 @deal.ensure(
-    lambda keyspace, pass_length, result: len(result) == pass_length,
+    lambda charspace, pass_length, result: len(result) == pass_length,
     message="function return value len must equal requested pass_length.",
 )
 @deal.ensure(
-    lambda keyspace, pass_length, result: all(
-        char in "".join(keyspace) for char in result
+    lambda charspace, pass_length, result: all(
+        char in "".join(charspace) for char in result
     ),
     message="function return value must be "
-    "composed of characters in the keyspace",
+    "composed of characters in the charspace",
 )
-def generate(keyspace: Set[str] | FrozenSet[str], pass_length: int) -> str:
+def generate(charspace: Set[str] | FrozenSet[str], pass_length: int) -> str:
     """Return a cryptographically secure password of length pass_length using
-    characters only from the given keyspace. Max pass_length is 512."""
+    characters only from the given charspace. Max pass_length is 512."""
     return "".join(
-        secrets.choice("".join(keyspace)) for i in range(pass_length)
+        secrets.choice("".join(charspace)) for i in range(pass_length)
     )
