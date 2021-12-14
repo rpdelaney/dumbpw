abd0c9e88fbccd23391c6fbd1cb54e6346bfa13a
Ryan Delaney <ryan.patrick.delaney@protonmail.com>
2021-12-14 11:16:23 -0500

    Rename charspace arg to charset (again?)

--- dumbpw/pwgen.py
+++ dumbpw/pwgen.py
@@ -8,34 +8,34 @@ import deal
 @deal.pure
 @deal.raises()
 @deal.pre(
-    validator=lambda charspace, pass_length: pass_length > 0,
+    validator=lambda charset, pass_length: pass_length > 0,
     message="pass_length must be greater than zero.",
     exception=ValueError,
 )
 @deal.pre(
-    validator=lambda charspace, pass_length: pass_length <= 512,
+    validator=lambda charset, pass_length: pass_length <= 512,
     message="pass_length cannot be greater than 512.",
     exception=ValueError,
 )
 @deal.pre(
-    validator=lambda charspace, pass_length: len("".join(charspace)) > 0,
-    message="charspace must have positive len.",
+    validator=lambda charset, pass_length: len("".join(charset)) > 0,
+    message="charset must have positive len.",
     exception=ValueError,
 )
 @deal.ensure(
-    lambda charspace, pass_length, result: len(result) == pass_length,
+    lambda charset, pass_length, result: len(result) == pass_length,
     message="function return value len must equal requested pass_length.",
 )
 @deal.ensure(
-    lambda charspace, pass_length, result: all(
-        char in "".join(charspace) for char in result
+    lambda charset, pass_length, result: all(
+        char in "".join(charset) for char in result
     ),
     message="function return value must be "
-    "composed of characters in the charspace",
+    "composed of characters in the charset",
 )
-def generate(charspace: Set[str] | FrozenSet[str], pass_length: int) -> str:
+def generate(charset: Set[str] | FrozenSet[str], pass_length: int) -> str:
     """Return a cryptographically secure password of length pass_length using
-    characters only from the given charspace. Max pass_length is 512."""
+    characters only from the given charset. Max pass_length is 512."""
     return "".join(
-        secrets.choice("".join(charspace)) for i in range(pass_length)
+        secrets.choice("".join(charset)) for i in range(pass_length)
     )
