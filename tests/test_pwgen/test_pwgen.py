0cc333d4887b62b559bf359c57ea0b07cc26e940
Ryan Delaney <ryan.patrick.delaney@protonmail.com>
2022-01-10 13:41:09 -0500

    Add test runner for pwgen.search()

--- tests/test_pwgen/test_pwgen.py
+++ tests/test_pwgen/test_pwgen.py
@@ -8,3 +8,10 @@ import dumbpw.pwgen as pwgen
 )
 def test_generate(case: deal.TestCase) -> None:
     case()
+
+
+@deal.cases(
+    func=pwgen.search,
+)
+def test_search(case: deal.TestCase) -> None:
+    case()
