3fce3c4fdd4fc8e67d3a2b53290d15149a99a633
Ryan Delaney <ryan.patrick.delaney+github@protonmail.com>
2022-04-07 12:56:53 -0400

    Remove tests for string properties
    If I can't trust the standard library 'string' type, then I don't even
    know what's real anymore

--- tests/test_candidate/test_candidate.py
+++ tests/test_candidate/test_candidate.py
@@ -44,39 +44,3 @@ def test_has_no_repeating(text):
     )
 
     assert not cd.has_repeating
-
-
-@given(strats.text())
-def test_len(text):
-    cd = Candidate(text)
-
-    assert len(cd) == len(text)
-
-
-@given(strats.text())
-def test_str(text):
-    cd = Candidate(text)
-
-    assert str(cd) == text
-
-
-@given(strats.text())
-def test_repr(text):
-    cd = Candidate(text)
-
-    assert repr(cd) == f'dumbpw.candidate.Candidate("{text}")'
-
-
-@given(strats.text(), strats.text())
-def test_eq(text1, text2):
-    cd1 = Candidate(text1)
-    cd2 = Candidate(text2)
-
-    assert cd1 == text1
-    assert cd1 is not text1
-    assert (cd1 == cd2) is (text1 == text2)
-
-
-@given(strats.text(), strats.text())
-def test_concatenateable(text1, text2):
-    assert Candidate(text1) + Candidate(text2) == text1 + text2
