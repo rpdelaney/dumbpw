55c2bd0f7024a421c4072efe9a391046523a8e76
Ryan Delaney <ryan.patrick.delaney@protonmail.com>
2022-07-29 18:28:28 -0400

    Use simpler deduplication method

--- dumbpw/charspace.py
+++ dumbpw/charspace.py
@@ -32,6 +32,4 @@ class Charspace:
         """De-duplicate the base charset, remove characters that are in the
         blocklist, and return a charset as a string.
         """
-        return "".join(
-            {char for char in self.base_charset if char not in self.blocklist}
-        )
+        return "".join(set(self.base_charset) - set(self.blocklist))
