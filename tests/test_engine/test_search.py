f9bfe88d963f0518c9c12f8865ac494c4707bc9b
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2022-10-24 17:02:47 -0400

    Try blocking the health check in the problem area

--- tests/test_engine/test_search.py
+++ tests/test_engine/test_search.py
@@ -1,9 +1,18 @@
+import os
 import string
 
 import deal
+from hypothesis import HealthCheck, settings
 from hypothesis import strategies as strats
 
 import dumbpw.engine as engine
+
+settings.register_profile(
+    "CI",
+    suppress_health_check=(HealthCheck.too_slow,),
+)
+if os.getenv("CI", False):
+    settings.load_profile("CI")
 
 
 @deal.cases(
