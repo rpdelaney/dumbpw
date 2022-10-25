05032936f6c8f77656e390ddd7eebbf4a046c0b3
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2022-10-25 10:30:35 -0400

    Also load CI profile in test_engine
    DRY :(

--- tests/test_engine/test_generate.py
+++ tests/test_engine/test_generate.py
@@ -1,6 +1,16 @@
+import os
+
 import deal
+from hypothesis import HealthCheck, settings
 
 from dumbpw.engine import generate
+
+settings.register_profile(
+    "CI",
+    suppress_health_check=(HealthCheck.too_slow,),
+)
+if os.getenv("CI", False):
+    settings.load_profile("CI")
 
 
 @deal.cases(
