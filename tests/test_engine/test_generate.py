89b893bb852a7db04b9dc373ace433e0e82458a1
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2022-10-27 16:18:10 -0400

    Why won't this health check die?

--- tests/test_engine/test_generate.py
+++ tests/test_engine/test_generate.py
@@ -1,5 +1,3 @@
-import os
-
 import deal
 from hypothesis import HealthCheck, settings
 
@@ -9,8 +7,7 @@ settings.register_profile(
     "CI",
     suppress_health_check=(HealthCheck.too_slow,),
 )
-if os.getenv("CI", False):
-    settings.load_profile("CI")
+settings.load_profile("CI")
 
 
 @deal.cases(
--- tests/test_engine/test_search.py
+++ tests/test_engine/test_search.py
@@ -1,4 +1,3 @@
-import os
 import string
 
 import deal
@@ -11,8 +10,7 @@ settings.register_profile(
     "CI",
     suppress_health_check=(HealthCheck.too_slow,),
 )
-if os.getenv("CI", False):
-    settings.load_profile("CI")
+settings.load_profile("CI")
 
 
 @deal.cases(
