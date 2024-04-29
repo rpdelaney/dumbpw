c0e0e4254f014dba5eb03f9429999227dbf6f628
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2024-04-29 16:40:18 -0400

    Fix ruff errors in charspace.py

--- dumbpw/charspace.py
+++ dumbpw/charspace.py
@@ -1,9 +1,8 @@
+"""Define a dataclass for rendering the character search space."""
+
 from dataclasses import dataclass
 
 import deal
-
-deal.activate()
-deal.module_load(deal.pure)
 
 from .constants import (
     DEFAULT_DIGITS,
@@ -13,8 +12,14 @@ from .constants import (
 )
 
 
+deal.activate()
+deal.module_load(deal.pure)
+
+
 @dataclass(frozen=True)
 class Charspace:
+    """Define an object that calculates and renders character space."""
+
     blocklist: str = ""
     digits: str = DEFAULT_DIGITS
     extras: str = DEFAULT_EXTRAS
@@ -24,12 +29,15 @@ class Charspace:
     @property
     @deal.pure
     def base_charset(self) -> set[str]:
+        """Calculate the base (unprocessed) charset."""
         return set(self.lowers + self.uppers + self.digits + self.extras)
 
     @property
     @deal.pure
     def charset(self) -> set[str]:
-        """De-duplicate the base charset, remove characters that are in the
-        blocklist, and return a charset as a string.
+        """De-duplicate the base charset.
+
+        Remove characters that are in the blocklist, and return a
+        charset as a string.
         """
         return set(self.base_charset) - set(self.blocklist)
