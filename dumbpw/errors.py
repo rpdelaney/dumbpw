433af9c99c1f311f97c6a9a066eb7a97805e716d
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2026-02-11 10:45:42 -0500

    Add DumbExitCode enum

--- dumbpw/errors.py
+++ dumbpw/errors.py
@@ -1,5 +1,8 @@
 """Define exceptions and error states for dumbpw."""
 
+import os
+from enum import IntEnum
+
 
 class DumbError(Exception):
     """Generic exception."""
@@ -7,3 +10,15 @@ class DumbError(Exception):
 
 class DumbValueError(ValueError, DumbError):
     """ValueError for own objects."""
+
+
+class DumbExitCode(IntEnum):
+    """Exit status codes corresponding to 'sysexits.h'."""
+
+    CONFIG = os.EX_CONFIG
+    DATA_ERR = os.EX_DATAERR
+    IO_ERR = os.EX_IOERR
+    NO_INPUT = os.EX_NOINPUT
+    OK = os.EX_OK
+    SOFTWARE = os.EX_SOFTWARE
+    USAGE = os.EX_USAGE
