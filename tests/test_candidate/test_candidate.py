ddf091dceffedbaa38c3b08e001271ea248d6c9e
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2026-02-19 16:57:33 -0500

    Rename candidate test functions
    Easier discovery this way

--- tests/test_candidate/test_candidate.py
+++ tests/test_candidate/test_candidate.py
@@ -4,47 +4,47 @@ from hypothesis import given
 from dumbpw.candidate import Candidate
 
 
-def test_is_string():
+def test_candidate_is_string():
     cd = Candidate("")
 
     assert isinstance(cd, str)
 
 
-def test_zero_len_duplicates():
+def test_candidate_zero_len_duplicates():
     cd = Candidate("")
 
     assert not cd.has_duplicates
 
 
 @given(strats.text(min_size=1))
-def test_has_duplicates(text):
+def test_candidate_has_duplicates(text):
     cd = Candidate(text + text)
 
     assert cd.has_duplicates
 
 
 @given(strats.text(min_size=1))
-def test_has_no_duplicates(text):
+def test_candidate_has_no_duplicates(text):
     cd = Candidate("".join(set(text)))
 
     assert not cd.has_duplicates
 
 
-def test_zero_len_repeating():
+def test_candidate_zero_len_repeating():
     cd = Candidate("")
 
     assert not cd.has_repeating
 
 
 @given(strats.text(min_size=1))
-def test_has_repeating(text):
+def test_candidate_has_repeating(text):
     cd = Candidate(sorted(text + text))
 
     assert cd.has_repeating
 
 
 @given(strats.text(min_size=1))
-def test_has_no_repeating(text):
+def test_candidate_has_no_repeating(text):
     cd = Candidate(
         "".join(set(text)),
     )
@@ -53,7 +53,7 @@ def test_has_no_repeating(text):
 
 
 @given(strats.text())
-def test_copy_method(text):
+def test_candidate_copy_method(text):
     cd = Candidate(text)
     cp = cd.copy()
 
